fede9df274290b9716e23e5e3145c9e0894c3be6
adrhc <adrhc@outlook.com>
2020-05-15 12:18:49 +0300

    init

---
+++ src/main/java/ro/go/adrhc/springbootkstreamstutorial/SpringBootKstreamsTutorialApplication.java
@@ -0,0 +1,11 @@
+package ro.go.adrhc.springbootkstreamstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKstreamsTutorialApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKstreamsTutorialApplication.class, args);
+	}
+}
---
+++ src/test/java/ro/go/adrhc/springbootkstreamstutorial/SpringBootKstreamsTutorialApplicationTests.java
@@ -0,0 +1,11 @@
+package ro.go.adrhc.springbootkstreamstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKstreamsTutorialApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
