dcff8026ea61122a27a34bb292b07e5e52f27276
adrhc <adrhc@outlook.com>
2020-05-18 12:23:17 +0300

    @Profile("!test")

--- src/main/java/ro/go/adrhc/springbootkstreamstutorial/config/TopicsConfig.java
+++ src/main/java/ro/go/adrhc/springbootkstreamstutorial/config/TopicsConfig.java
@@ -3,11 +3,13 @@ package ro.go.adrhc.springbootkstreamstutorial.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.TopicBuilder;
 
 @EnableKafkaStreams
 @Configuration
+@Profile("!test")
 public class TopicsConfig {
 	private final TopicsProperties properties;
 
