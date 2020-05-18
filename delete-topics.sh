fe3f314d167e6a8c3d8157873c67885e71a1e7b8
adrhc <adrhc@outlook.com>
2020-05-18 13:37:24 +0300

    schema deletion

--- delete-topics.sh
+++ delete-topics.sh
@@ -11,5 +11,11 @@ fi
 
 KDELETE="kafka-topics --delete --zookeeper 127.0.0.1:2181 --topic"
 
+# topic deletion
 $KDELETE sbkst.commands.v2
 $KDELETE sbkst.client-profiles.v2
+
+# schema deletion
+curl -X DELETE http://localhost:8081/subjects/sbkst.client-profiles.v2-value
+curl -X DELETE http://localhost:8081/subjects/sbkst-sbkst.client-profiles.v2-changelog-value
+curl -X DELETE http://localhost:8081/subjects/sbkst-sbkst.client-profiles.v2-store-changelog-value
