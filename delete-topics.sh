8944695e07db4de2096a2dcf7475609938ca816c
adrhc <adrhc@outlook.com>
2020-05-18 11:38:53 +0300

    delete-topics.sh

--- delete-topics.sh
+++ delete-topics.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-shopt -s expand_aliases
-source ~/.bash_aliases
 
-echo "KAFKA_HOME: $KAFKA_HOME"
-if [ "$KAFKA_HOME" == "" ]; then
-	echo "set KAFKA_HOME OS-environment variable!"
+# shopt -s expand_aliases
+# source ~/.bash_aliases
+# cd /home/adr/tools/kafka/kafka_2.12-2.4.0
+# alias kdelete='bin/kafka-topics.sh --delete --zookeeper 127.0.0.1:2181 --topic'
+
+echo "CONFLUENT_HOME: $CONFLUENT_HOME"
+if [ "$CONFLUENT_HOME" == "" ]; then
+	echo "set CONFLUENT_HOME OS-environment variable!"
 	exit 1
 fi
 
-KDELETE="$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper 127.0.0.1:2181 --topic"
+KDELETE="kafka-topics --delete --zookeeper 127.0.0.1:2181 --topic"
 
 $KDELETE sbkst.commands.v2
 $KDELETE sbkst.client-profiles.v2
-
