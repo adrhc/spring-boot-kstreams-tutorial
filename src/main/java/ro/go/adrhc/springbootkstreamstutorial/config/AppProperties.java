aaffa3c5d6daef2c963d1f3fb5351c9ff59d5ea7
adrhc <adrhc@outlook.com>
2020-05-26 12:17:30 +0300

    AppProperties comments

--- src/main/java/ro/go/adrhc/springbootkstreamstutorial/config/AppProperties.java
+++ src/main/java/ro/go/adrhc/springbootkstreamstutorial/config/AppProperties.java
@@ -10,7 +10,21 @@ import org.springframework.stereotype.Component;
 @Data
 @NoArgsConstructor
 public class AppProperties {
+	/*
+	 * this should pertain to "adapters"
+	 * could be used by monitoring, reporting, logging, audit
+	 */
 	private String version;
-	private int dailyGrace;
+	/*
+	 * This should be loaded from a store along with amounts spent.
+	 * If configured than should pertain to application's "core" if is
+	 * part of the domain-model or "adapters" if related only to reporting.
+	 */
 	private String currency;
+	/*
+	 * This is related to topology configuration so should be in
+	 * something named like e.g. ...adapters.config.TopologiesProperties.
+	 * Could also pertain to domain-model in which case should stay in application's "core".
+	 */
+	private int dailyGrace;
 }
